02ecedb38ccf1f58f3889990e02fd01c0e8a463c
David LISSOCK <licksontravis@gmail.com>
2022-09-07 22:40:23 +0100

    refactor: Make first overall changes
    - app.py : complete code
    - config.py : add database url
    - forms.py : add forms validators
    - models.py : file creation
    - requirements.txt : update dependencies

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from flask_wtf import Form
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm as Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms.validators import DataRequired, AnyOf, URL, Regexp
 
 class ShowForm(Form):
     artist_id = StringField(
@@ -83,7 +84,11 @@ class VenueForm(Form):
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone'
+        'phone', validators=[DataRequired(),
+                             Regexp("\b[\d]{3}-[\d]{3}-[\d]{4}\b",
+                             message="Format mandatory : xxx-xxx-xxxx"
+                                   )
+                            ]
     )
     image_link = StringField(
         'image_link'
@@ -117,7 +122,7 @@ class VenueForm(Form):
         'facebook_link', validators=[URL()]
     )
     website_link = StringField(
-        'website_link'
+        'website_link', validators=[URL()]
     )
 
     seeking_talent = BooleanField( 'seeking_talent' )
@@ -192,8 +197,11 @@ class ArtistForm(Form):
         ]
     )
     phone = StringField(
-        # TODO implement validation logic for state
-        'phone'
+        'phone', validators=[DataRequired(),
+                             Regexp("\b[\d]{3}-[\d]{3}-[\d]{4}\b",
+                             message="Format mandatory : xxx-xxx-xxxx"
+                                   )
+                            ]
     )
     image_link = StringField(
         'image_link'
@@ -228,7 +236,7 @@ class ArtistForm(Form):
      )
 
     website_link = StringField(
-        'website_link'
+        'website_link', validators=[URL()]
      )
 
     seeking_venue = BooleanField( 'seeking_venue' )
